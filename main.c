b3c1d54efff5c15acfac6ee2af6304f8bc35fd83
Earthman <960218mark@gmail.com>
2025-10-25 19:05:49 +0800

    change variable names and implement parsing for `MUL_EXPR` and `UNARY_EXPR`

--- main.c
+++ main.c
@@ -11,17 +11,17 @@ For the language grammar, please refer to Grammar section on the github page:
 #define MAX_LENGTH 200
 typedef enum {
 	ASSIGN, ADD, SUB, MUL, DIV, REM, PREINC, PREDEC, POSTINC, POSTDEC, IDENTIFIER, CONSTANT, LPAR, RPAR, PLUS, MINUS, END
-} Kind;
+} Type;
 typedef enum {
 	STMT, EXPR, ASSIGN_EXPR, ADD_EXPR, MUL_EXPR, UNARY_EXPR, POSTFIX_EXPR, PRI_EXPR
 } GrammarState;
 typedef struct TokenUnit {
-	Kind kind;
+	Type type;
 	int val; // record the integer value or variable name
 	struct TokenUnit *next;
 } Token;
 typedef struct ASTUnit {
-	Kind kind;
+	Type type;
 	int val; // record the integer value or variable name
 	struct ASTUnit *lhs, *mid, *rhs;
 } AST;
@@ -38,35 +38,35 @@ typedef struct ASTUnit {
 	exit(0);\
 }
 // You may set DEBUG=1 to debug. Remember setting back to 0 before submit.
-#define DEBUG 0
+#define DEBUG 1
 // Split the input char array into token linked list.
 Token *lexer(const char *in);
 // Create a new token.
-Token *new_token(Kind kind, int val);
+Token *new_token(Type type, int val);
 // Translate a token linked list into array, return its length.
 size_t token_list_to_arr(Token **head);
 // Parse the token array. Return the constructed AST.
-AST *parser(Token *arr, size_t len);
+AST *parser(Token *token_array, size_t length);
 // Parse the token array. Return the constructed AST.
-AST *parse(Token *arr, int l, int r, GrammarState S);
+AST *parse(Token *token_array, int start, int end, GrammarState state);
 // Create a new AST node.
-AST *new_AST(Kind kind, int val);
+AST *new_AST(Type type, int val);
 // Find the location of next token that fits the condition(cond). Return -1 if not found. Search direction from start to end.
-int findNextSection(Token *arr, int start, int end, int (*cond)(Kind));
-// Return 1 if kind is ASSIGN.
-int condASSIGN(Kind kind);
-// Return 1 if kind is ADD or SUB.
-int condADD(Kind kind);
-// Return 1 if kind is MUL, DIV, or REM.
-int condMUL(Kind kind);
-// Return 1 if kind is RPAR.
-int condRPAR(Kind kind);
+int findNextSection(Token *token_array, int start, int end, int (*cond)(Type));
+// Return 1 if type is ASSIGN.
+int condASSIGN(Type type);
+// Return 1 if type is ADD or SUB.
+int condADD(Type type);
+// Return 1 if type is MUL, DIV, or REM.
+int condMUL(Type type);
+// Return 1 if type is RPAR.
+int condRPAR(Type type);
 // Check if the AST is semantically right. This function will call err() automatically if check failed.
-void semantic_check(AST *now);
+void semantic_check(AST *current);
 // Generate ASM code.
 void codegen(AST *root);
 // Free the whole AST.
-void freeAST(AST *now);
+void freeAST(AST *current);
 
 /// debug interfaces
 
@@ -93,67 +93,67 @@ int main() {
 
 Token *lexer(const char *in) {
 	Token *head = NULL;
-	Token **now = &head;
+	Token **current = &head;
 	for (int i = 0; in[i]; i++) {
 		if (isspace(in[i])) // ignore space characters
 			continue;
 		else if (isdigit(in[i])) {
-			(*now) = new_token(CONSTANT, atoi(in + i));
+			(*current) = new_token(CONSTANT, atoi(in + i));
 			while (in[i+1] && isdigit(in[i+1])) i++;
 		}
 		else if ('x' <= in[i] && in[i] <= 'z') // variable
-			(*now) = new_token(IDENTIFIER, in[i]);
+			(*current) = new_token(IDENTIFIER, in[i]);
 		else switch (in[i]) {
 			case '=':
-				(*now) = new_token(ASSIGN, 0);
+				(*current) = new_token(ASSIGN, 0);
 				break;
 			case '+':
 				if (in[i+1] && in[i+1] == '+') {
 					i++;
 					// In lexer scope, all "++" will be labeled as PREINC.
-					(*now) = new_token(PREINC, 0);
+					(*current) = new_token(PREINC, 0);
 				}
 				// In lexer scope, all single "+" will be labeled as PLUS.
-				else (*now) = new_token(PLUS, 0);
+				else (*current) = new_token(PLUS, 0);
 				break;
 			case '-':
 				if (in[i+1] && in[i+1] == '-') {
 					i++;
 					// In lexer scope, all "--" will be labeled as PREDEC.
-					(*now) = new_token(PREDEC, 0);
+					(*current) = new_token(PREDEC, 0);
 				}
 				// In lexer scope, all single "-" will be labeled as MINUS.
-				else (*now) = new_token(MINUS, 0);
+				else (*current) = new_token(MINUS, 0);
 				break;
 			case '*':
-				(*now) = new_token(MUL, 0);
+				(*current) = new_token(MUL, 0);
 				break;
 			case '/':
-				(*now) = new_token(DIV, 0);
+				(*current) = new_token(DIV, 0);
 				break;
 			case '%':
-				(*now) = new_token(REM, 0);
+				(*current) = new_token(REM, 0);
 				break;
 			case '(':
-				(*now) = new_token(LPAR, 0);
+				(*current) = new_token(LPAR, 0);
 				break;
 			case ')':
-				(*now) = new_token(RPAR, 0);
+				(*current) = new_token(RPAR, 0);
 				break;
 			case ';':
-				(*now) = new_token(END, 0);
+				(*current) = new_token(END, 0);
 				break;
 			default:
 				err("Unexpected character.");
 		}
-		now = &((*now)->next);
+		current = &((*current)->next);
 	}
 	return head;
 }
 
-Token *new_token(Kind kind, int val) {
+Token *new_token(Type type, int val) {
 	Token *res = (Token*)malloc(sizeof(Token));
-	res->kind = kind;
+	res->type = type;
 	res->val = val;
 	res->next = NULL;
 	return res;
@@ -161,90 +161,99 @@ Token *new_token(Kind kind, int val) {
 
 size_t token_list_to_arr(Token **head) {
 	size_t res;
-	Token *now = (*head), *del;
-	for (res = 0; now != NULL; res++) now = now->next;
-	now = (*head);
+	Token *current = (*head), *del;
+	for (res = 0; current != NULL; res++) current = current->next;
+	current = (*head);
 	if (res != 0) (*head) = (Token*)malloc(sizeof(Token) * res);
 	for (int i = 0; i < res; i++) {
-		(*head)[i] = (*now);
-		del = now;
-		now = now->next;
+		(*head)[i] = (*current);
+		del = current;
+		current = current->next;
 		free(del);
 	}
 	return res;
 }
 
-AST *parser(Token *arr, size_t len) {
-	for (int i = 1; i < len; i++) {
+AST *parser(Token *token_array, size_t length) {
+	for (int i = 1; i < length; i++) {
 		// correctly identify "ADD" and "SUB"
-		if (arr[i].kind == PLUS || arr[i].kind == MINUS) {
-			switch (arr[i - 1].kind) {
+		if (token_array[i].type == PLUS || token_array[i].type == MINUS) {
+			switch (token_array[i - 1].type) {
 				case PREINC:
 				case PREDEC:
 				case IDENTIFIER:
 				case CONSTANT:
 				case RPAR:
-					arr[i].kind = arr[i].kind - PLUS + ADD;
+					token_array[i].type = token_array[i].type - PLUS + ADD;
 				default: break;
 			}
 		}
 	}
-	return parse(arr, 0, len - 1, STMT);
+	return parse(token_array, 0, length - 1, STMT);
 }
 
-AST *parse(Token *arr, int l, int r, GrammarState S) {
-	AST *now = NULL;
-	if (l > r)
+AST *parse(Token *token_array, int start, int end, GrammarState state) {
+	AST *current = NULL;
+	if (start > end)
 		err("Unexpected parsing range.");
-	int nxt;
-	switch (S) {
+	int next;
+	switch (state) {
 		case STMT:
-			if (l == r && arr[l].kind == END)
+			if (start == end && token_array[start].type == END)
 				return NULL;
-			else if (arr[r].kind == END)
-				return parse(arr, l, r - 1, EXPR);
+			else if (token_array[end].type == END)
+				return parse(token_array, start, end - 1, EXPR);
 			else err("Expected \';\' at the end of line.");
 		case EXPR:
-			return parse(arr, l, r, ASSIGN_EXPR);
+			return parse(token_array, start, end, ASSIGN_EXPR);
 		case ASSIGN_EXPR:
-			if ((nxt = findNextSection(arr, l, r, condASSIGN)) != -1) {
-				now = new_AST(arr[nxt].kind, 0);
-				now->lhs = parse(arr, l, nxt - 1, UNARY_EXPR);
-				now->rhs = parse(arr, nxt + 1, r, ASSIGN_EXPR);
-				return now;
+			if ((next = findNextSection(token_array, start, end, condASSIGN)) != -1) {
+				current = new_AST(token_array[next].type, 0);
+				current->lhs = parse(token_array, start, next - 1, UNARY_EXPR);
+				current->rhs = parse(token_array, next + 1, end, ASSIGN_EXPR);
+				return current;
 			}
-			return parse(arr, l, r, ADD_EXPR);
+			return parse(token_array, start, end, ADD_EXPR);
 		case ADD_EXPR:
-			if((nxt = findNextSection(arr, r, l, condADD)) != -1) {
-				now = new_AST(arr[nxt].kind, 0);
-				now->lhs = parse(arr, l, nxt - 1, ADD_EXPR);
-				now->rhs = parse(arr, nxt + 1, r, MUL_EXPR);
-				return now;
+			if((next = findNextSection(token_array, end, start, condADD)) != -1) {
+				current = new_AST(token_array[next].type, 0);
+				current->lhs = parse(token_array, start, next - 1, ADD_EXPR);
+				current->rhs = parse(token_array, next + 1, end, MUL_EXPR);
+				return current;
 			}
-			return parse(arr, l, r, MUL_EXPR);
+			return parse(token_array, start, end, MUL_EXPR);
 		case MUL_EXPR:
-			// TODO: Implement MUL_EXPR.
-			// hint: Take ADD_EXPR as reference.
+			if((next = findNextSection(token_array, end, start, condMUL)) != -1) {
+				current = new_AST(token_array[next].type, 0);
+				current->lhs = parse(token_array, start, next - 1, MUL_EXPR);
+				current->rhs = parse(token_array, next + 1, end, UNARY_EXPR);
+				return current;
+			}
+			return parse(token_array, start, end, UNARY_EXPR);
 		case UNARY_EXPR:
-			// TODO: Implement UNARY_EXPR.
-			// hint: Take POSTFIX_EXPR as reference.
+			if (token_array[start].type == PREINC || token_array[start].type == PREDEC || token_array[start].type == PLUS || token_array[start].type == MINUS) {
+				current = new_AST(token_array[start].type, 0);
+				current->mid = parse(token_array, start, end - 1, UNARY_EXPR);
+				return current;
+			}
+			return parse(token_array, start, end, POSTFIX_EXPR);
 		case POSTFIX_EXPR:
-			if (arr[r].kind == PREINC || arr[r].kind == PREDEC) {
+			if (token_array[end].type == PREINC || token_array[end].type == PREDEC) {
 				// translate "PREINC", "PREDEC" into "POSTINC", "POSTDEC"
-				now = new_AST(arr[r].kind - PREINC + POSTINC, 0);
-				now->mid = parse(arr, l, r - 1, POSTFIX_EXPR);
-				return now;
+				current = new_AST(token_array[end].type - PREINC + POSTINC, 0);
+				current->mid = parse(token_array, start, end - 1, POSTFIX_EXPR);
+				return current;
 			}
-			return parse(arr, l, r, PRI_EXPR);
+			return parse(token_array, start, end, PRI_EXPR);
 		case PRI_EXPR:
-			if (findNextSection(arr, l, r, condRPAR) == r) {
-				now = new_AST(LPAR, 0);
-				now->mid = parse(arr, l + 1, r - 1, EXPR);
-				return now;
+			if (findNextSection(token_array, start, end, condRPAR) == end) {
+				current = new_AST(LPAR, 0);
+				current->mid = parse(token_array, start + 1, end - 1, EXPR);
+				return current;
 			}
-			if (l == r) {
-				if (arr[l].kind == IDENTIFIER || arr[l].kind == CONSTANT)
-					return new_AST(arr[l].kind, arr[l].val);
+			if (start == end) {
+				if (token_array[start].type == IDENTIFIER || token_array[start].type == CONSTANT)
+					return new_AST(token_array[start].type, token_array[start].val);
 				err("Unexpected token during parsing.");
 			}
 			err("No token left for parsing.");
@@ -253,48 +262,48 @@ AST *parse(Token *arr, int l, int r, GrammarState S) {
 	}
 }
 
-AST *new_AST(Kind kind, int val) {
+AST *new_AST(Type type, int val) {
 	AST *res = (AST*)malloc(sizeof(AST));
-	res->kind = kind;
+	res->type = type;
 	res->val = val;
 	res->lhs = res->mid = res->rhs = NULL;
 	return res;
 }
 
-int findNextSection(Token *arr, int start, int end, int (*cond)(Kind)) {
+int findNextSection(Token *arr, int start, int end, int (*cond)(Type)) {
 	int par = 0;
 	int d = (start < end) ? 1 : -1;
 	for (int i = start; (start < end) ? (i <= end) : (i >= end); i += d) {
-		if (arr[i].kind == LPAR) par++;
-		if (arr[i].kind == RPAR) par--;
-		if (par == 0 && cond(arr[i].kind) == 1) return i;
+		if (arr[i].type == LPAR) par++;
+		if (arr[i].type == RPAR) par--;
+		if (par == 0 && cond(arr[i].type) == 1) return i;
 	}
 	return -1;
 }
 
-int condASSIGN(Kind kind) {
-	return kind == ASSIGN;
+int condASSIGN(Type type) {
+	return type == ASSIGN;
 }
 
-int condADD(Kind kind) {
-	return kind == ADD || kind == SUB;
+int condADD(Type type) {
+	return type == ADD || type == SUB;
 }
 
-int condMUL(Kind kind) {
-	return kind == MUL || kind == DIV || kind == REM;
+int condMUL(Type type) {
+	return type == MUL || type == DIV || type == REM;
 }
 
-int condRPAR(Kind kind) {
-	return kind == RPAR;
+int condRPAR(Type type) {
+	return type == RPAR;
 }
 
-void semantic_check(AST *now) {
-	if (now == NULL) return;
+void semantic_check(AST *current) {
+	if (current == NULL) return;
 	// Left operand of '=' must be an identifier or identifier with one or more parentheses.
-	if (now->kind == ASSIGN) {
-		AST *tmp = now->lhs;
-		while (tmp->kind == LPAR) tmp = tmp->mid;
-		if (tmp->kind != IDENTIFIER)
+	if (current->type == ASSIGN) {
+		AST *tmp = current->lhs;
+		while (tmp->type == LPAR) tmp = tmp->mid;
+		if (tmp->type != IDENTIFIER)
 			err("Lvalue is required as left operand of assignment.");
 	}
 	// Operand of INC/DEC must be an identifier or identifier with one or more parentheses.
@@ -308,25 +317,25 @@ void codegen(AST *root) {
 	// You may modify the function parameter or the return type, even the whole structure as you wish.
 }
 
-void freeAST(AST *now) {
-	if (now == NULL) return;
-	freeAST(now->lhs);
-	freeAST(now->mid);
-	freeAST(now->rhs);
-	free(now);
+void freeAST(AST *current) {
+	if (current == NULL) return;
+	freeAST(current->lhs);
+	freeAST(current->mid);
+	freeAST(current->rhs);
+	free(current);
 }
 
 void token_print(Token *in, size_t len) {
-	const static char KindName[][20] = {
+	const static char typeName[][20] = {
 		"Assign", "Add", "Sub", "Mul", "Div", "Rem", "Inc", "Dec", "Inc", "Dec", "Identifier", "Constant", "LPar", "RPar", "Plus", "Minus", "End"
 	};
-	const static char KindSymbol[][20] = {
+	const static char typeSymbol[][20] = {
 		"'='", "'+'", "'-'", "'*'", "'/'", "'%'", "\"++\"", "\"--\"", "\"++\"", "\"--\"", "", "", "'('", "')'", "'+'", "'-'"
 	};
 	const static char format_str[] = "<Index = %3d>: %-10s, %-6s = %s\n";
 	const static char format_int[] = "<Index = %3d>: %-10s, %-6s = %d\n";
 	for(int i = 0; i < len; i++) {
-		switch(in[i].kind) {
+		switch(in[i].type) {
 			case LPAR:
 			case RPAR:
 			case PREINC:
@@ -339,16 +348,16 @@ void token_print(Token *in, size_t len) {
 			case ASSIGN:
 			case PLUS:
 			case MINUS:
-				fprintf(stderr, format_str, i, KindName[in[i].kind], "symbol", KindSymbol[in[i].kind]);
+				fprintf(stderr, format_str, i, typeName[in[i].type], "symbol", typeSymbol[in[i].type]);
 				break;
 			case CONSTANT:
-				fprintf(stderr, format_int, i, KindName[in[i].kind], "value", in[i].val);
+				fprintf(stderr, format_int, i, typeName[in[i].type], "value", in[i].val);
 				break;
 			case IDENTIFIER:
-				fprintf(stderr, format_str, i, KindName[in[i].kind], "name", (char*)(&(in[i].val)));
+				fprintf(stderr, format_str, i, typeName[in[i].type], "name", (char*)(&(in[i].val)));
 				break;
 			case END:
-				fprintf(stderr, "<Index = %3d>: %-10s\n", i, KindName[in[i].kind]);
+				fprintf(stderr, "<Index = %3d>: %-10s\n", i, typeName[in[i].type]);
 				break;
 			default:
 				fputs("=== unknown token ===", stderr);
@@ -359,20 +368,20 @@ void token_print(Token *in, size_t len) {
 void AST_print(AST *head) {
 	static char indent_str[MAX_LENGTH] = "  ";
 	static int indent = 2;
-	const static char KindName[][20] = {
+	const static char typeName[][20] = {
 		"Assign", "Add", "Sub", "Mul", "Div", "Rem", "PreInc", "PreDec", "PostInc", "PostDec", "Identifier", "Constant", "Parentheses", "Parentheses", "Plus", "Minus"
 	};
 	const static char format[] = "%s\n";
 	const static char format_str[] = "%s, <%s = %s>\n";
 	const static char format_val[] = "%s, <%s = %d>\n";
 	if (head == NULL) return;
-	char *indent_now = indent_str + indent;
+	char *indent_current = indent_str + indent;
 	indent_str[indent - 1] = '-';
 	fprintf(stderr, "%s", indent_str);
 	indent_str[indent - 1] = ' ';
 	if (indent_str[indent - 2] == '`')
 		indent_str[indent - 2] = ' ';
-	switch (head->kind) {
+	switch (head->type) {
 		case ASSIGN:
 		case ADD:
 		case SUB:
@@ -387,23 +396,23 @@ void AST_print(AST *head) {
 		case RPAR:
 		case PLUS:
 		case MINUS:
-			fprintf(stderr, format, KindName[head->kind]);
+			fprintf(stderr, format, typeName[head->type]);
 			break;
 		case IDENTIFIER:
-			fprintf(stderr, format_str, KindName[head->kind], "name", (char*)&(head->val));
+			fprintf(stderr, format_str, typeName[head->type], "name", (char*)&(head->val));
 			break;
 		case CONSTANT:
-			fprintf(stderr, format_val, KindName[head->kind], "value", head->val);
+			fprintf(stderr, format_val, typeName[head->type], "value", head->val);
 			break;
 		default:
 			fputs("=== unknown AST type ===", stderr);
 	}
 	indent += 2;
-	strcpy(indent_now, "| ");
+	strcpy(indent_current, "| ");
 	AST_print(head->lhs);
-	strcpy(indent_now, "` ");
+	strcpy(indent_current, "` ");
 	AST_print(head->mid);
 	AST_print(head->rhs);
 	indent -= 2;
-	(*indent_now) = '\0';
+	(*indent_current) = '\0';
 }
